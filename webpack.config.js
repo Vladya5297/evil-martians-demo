851b9356a7d440683ee74428732cf631a0f70445
Vladya5297 <123dobro@mail.ru>
2022-11-19 23:30:53 +0300

    feat: initial config

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+    entry: "./src/index.tsx",
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use: ['ts-loader'],
+            },
+            {
+                test: /\.html$/,
+                use: ["html-loader"]
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1,
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader'
+                    }
+                ]
+            }
+        ]
+    },
+    resolve: {extensions: ['.tsx', '.ts', '.js']},
+    output: {
+        path: path.resolve(__dirname, "build"),
+        filename: "index.js"
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html'
+        })
+    ],
+    devtool: 'inline-source-map',
+    devServer: {
+        open: true,
+        port: 3000,
+    },
+};
